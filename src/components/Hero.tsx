12187d084c6050ed622b506323df90d87767c12b
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-12-05 13:31:27 -0700

    initial commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,56 @@
+import headshot from '/src/assets/Headshot.jpg'
+
+import Ripple from "@/components/ui/ripple";
+import HyperText from "@/components/ui/hyper-text";
+import ShineBorder from "@/components/ui/shine-border";
+
+import { motion } from "motion/react"
+
+import { IconMapPin, IconBrandGithub, IconBrandLinkedin, IconFileCv } from '@tabler/icons-react';
+
+export const Hero = () => {
+    return(
+        <section className="w-full bg-primary">
+            <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden md:shadow-xl">
+                <motion.div
+                    initial={{ y: -40, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1.5 }}
+                    className="flex flex-col justify-center items-center"
+                >
+                    <img src={headshot} className='z-10 rounded-full w-48 mb-12'/>
+                    <h1 className="z-10 whitespace-pre-wrap text-center text-6xl font-bold tracking-wider text-primaryAccent">Bailey Henderson</h1>
+                    <div className="flex gap-x-2 justify-center mt-2">
+                    <HyperText
+                        text="Front-End"
+                        duration={1400}
+                        className="text-2xl text-neonPink font-semibold"
+                    ></HyperText>
+                    <HyperText
+                        text="Developer"
+                        duration={1400}
+                        className="text-2xl text-neonPink font-semibold"
+                    ></HyperText>
+                    </div>
+                    <div className="z-10 flex gap-x-2 justify-center items-center mt-2">
+                        <IconMapPin stroke={1} className="text-neonPink"/>
+                        <p className="tracking-wide">Phoenix, Arizona</p>
+                    </div>
+                    <ShineBorder
+                        color={'#ff0055'}
+                        borderWidth={2} 
+                        className="flex justify-center items-center bg-transparent mt-8">
+                            <a href="https://github.com/BaileyKH" target="_blank"><IconBrandGithub stroke={1} className="w-10 h-10 text-primaryAccent hover:text-neonPink transition-colors duration-300 ease-in-out"/></a>
+                            <a href="www.linkedin.com/in/baileykh" target="_blank"><IconBrandLinkedin stroke={1} className="w-10 h-10 mx-4 text-primaryAccent hover:text-neonPink transition-colors duration-300 ease-in-out"/></a>
+                            <IconFileCv stroke={1} className="w-10 h-10 text-primaryAccent"/>
+                    </ShineBorder>
+                </motion.div>
+                <Ripple 
+                    mainCircleSize={800}
+                    mainCircleOpacity={.5}
+                    numCircles={12}
+                />
+            </div>
+        </section>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: ["class"],
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+  	extend: {
+  		fontFamily: {
+  			poppins: ['Poppins", sans-serif;']
+  		},
+  		colors: {
+  			primary: '#121212',
+  			neonBlue: '#0077ff',
+  			neonPink: '#ff0055',
+  			primaryAccent: '#FFFFFF'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		animation: {
+  			ripple: 'ripple var(--duration,4s) ease calc(var(--i, 0)*.2s) infinite',
+  			shine: 'shine var(--duration) infinite linear'
+  		},
+  		keyframes: {
+  			ripple: {
+  				'0%, 100%': {
+  					transform: 'translate(-50%, -50%) scale(1)'
+  				},
+  				'50%': {
+  					transform: 'translate(-50%, -50%) scale(0.9)'
+  				}
+  			},
+  			shine: {
+  				'0%': {
+  					'background-position': '0% 0%'
+  				},
+  				'50%': {
+  					'background-position': '100% 100%'
+  				},
+  				to: {
+  					'background-position': '0% 0%'
+  				}
+  			}
+  		}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+}
