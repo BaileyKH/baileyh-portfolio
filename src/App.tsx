118b003dfdc096261f4b7bd4b81c6ce95cd9cd26
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-12-09 14:02:58 -0700

    Update: Added components to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+
+import { Contact } from "./components/Contact"
+import { Experience } from "./components/Experience"
+import { Footer } from "./components/Footer"
 import { Hero } from "./components/Hero"
+import { Nav } from "./components/Nav"
+import { Projects } from "./components/Projects"
+import { Stack } from "./components/Stack"
 
 
 function App() {
 
   return (
     <>
+      {/* <Nav /> */}
       <Hero />
+      <Stack />
+      <Projects />
+      <Experience />
+      <Contact />
+      <Footer />
     </>
   )
 }
