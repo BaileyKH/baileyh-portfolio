8cf387a38d0ddc0263f1a7572e448db5b735cda3
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-12-09 11:38:50 -0700

    Updated: adjusted shine border ui

--- src/components/ui/shine-border.tsx
+++ src/components/ui/shine-border.tsx
@@ -39,7 +39,7 @@ export default function ShineBorder({
         } as React.CSSProperties
       }
       className={cn(
-        "relative min-h-[60px] w-fit min-w-max place-items-center rounded-[--border-radius] bg-white p-3 text-black dark:bg-black dark:text-white",
+        "relative min-h-[60px] w-fit min-w-max place-items-center rounded-[--border-radius] p-3 text-black dark:bg-black dark:text-white",
         className,
       )}
     >
