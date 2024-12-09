eaa43901bba90a1d351548765602388f6b747299
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-12-09 14:10:57 -0700

    Initial nav creation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,16 +3,12 @@
 export const Nav = () => {
     return(
         <nav className="fixed flex justify-center items-center w-full mt-4 z-10">
-            <div className="flex justify-between items-center w-96 px-2 py-1 rounded-md border border-white/20 bg-white/10 backdrop-blur-sm">
-                <div>
-                    <h1>BKH</h1>
-                </div>
-                <div>
-                    <a>About</a>
-                    <a>Projects</a>
-                    <a>Contact</a>
-                </div>
-            </div>
+            <ul className="flex justify-between items-center w-[96] px-2 py-1">
+                <li className="nav-list">About</li>
+                <li className="nav-list">Projects</li>
+                <li className="nav-list">Experience</li>
+                <li className="nav-list">Contact</li>
+            </ul>
         </nav>
     );
 }
